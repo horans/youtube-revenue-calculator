6a42c2cc764d4fb1ab826dfb8aa9210a28944beb
Zhiqian MA <horans@gmail.com>
2018-07-27 21:50:03 +0800

    initial release

--- README.md
+++ README.md
@@ -1,2 +1,71 @@
 # youtube-revenue-calculator
-Calculate the revenue of YouTube video/channel base on video view counts.
+
+"youtube revenue calculator", or "ytrc", will calculate the revenue of [YouTube](https://www.youtube.com/) video/channel, base on video view counts.
+Multiple options provided.
+
+## Install
+
+Upload `dist` folder to your server, eg.:
+
+```text
+//sample.com/ytrc/
+```
+
+## Usage
+
+Embed `iframe.html` into your page.
+
+```html
+<iframe src="//sample.com/ytrc/iframe.html" width="800" height="600"></iframe>
+```
+
+## Guide
+
+All of the options can by pass by URL query.
+When you change them in setting panel, the URL will update at the same time.
+Most of the options use "1/0" to present "on/off".
+(eg., `channel=1&cpm=1`)
+
+* `key`: Generate your own YouTube app [key](https://console.developers.google.com/)
+    * and change it in iframe `head` or pass by URL query.
+* `channel`: Default mode is for "video", turn this on for "channel".
+* `desc`: To skip description under heading, turn this off.
+* `link`: To manually input video views, turn this off.
+* `cpm`: To manually input CPM, turn this on.
+* `note`: To show note for CPM, turn this on.
+* `highlight`: To highlight result with YouTube red background, turn this on.
+* `detail`: To show detail for channel/video, turn this on.
+    * Disabled when `link` is off, or `highlight` is on.
+* `title`: You can override title with this.
+
+## Extra
+
+### Vendor
+
+* [axios](https://github.com/axios/axios)
+* [bootstrap](https://github.com/twbs/bootstrap)
+* [noUiSlider](https://github.com/leongersen/noUiSlider)
+* [pretty-checkbox.css](https://github.com/lokesh-coder/pretty-checkbox)
+* [url()](https://github.com/websanova/js-url)
+* [Vue](https://github.com/vuejs/vue)
+* [vue-nouislider](https://github.com/horans/vue-nouislider)
+* [Web Font Loader](https://github.com/typekit/webfontloader)
+
+### References
+
+* [SOCIAL BLADE](https://socialblade.com/youtube/youtube-money-calculator)
+* [Influencer MarketingHub](https://influencermarketinghub.com/youtube-money-calculator/)
+* [YTSC](https://subscribercounter.com/)
+* [YouTube API Reference](https://developers.google.com/youtube/v3/docs/)
+
+### Linter
+
+* HTML: [HTMLHint](https://github.com/yaniswang/HTMLHint)
+* CSS: [CSSLint](https://github.com/CSSLint/csslint)
+* JavaScript: [standard](https://github.com/standard/standard)
+
+### Change Log
+
+__180727__
+
+* initial release
---
+++ dist/asset/youtube-revenue.js
@@ -0,0 +1,265 @@
+/*****************************************************
+*  project: youtube revenue calculator               *
+*  description: main function                        *
+*  author: horans@gmail.com                          *
+*  url: github.com/horans/youtube-revenue-calculator *
+*  update: 180727                                    *
+*****************************************************/
+/* global Vue, WebFont, url, key, axios */
+/* eslint no-unused-vars: ["error", { "varsIgnorePattern": "ytrc" }] */
+
+/* bus */
+var bus = new Vue()
+
+/* font */
+WebFont.load({
+  google: { families: ['Roboto'] },
+  active: function () { bus.$emit('ytrc.font') },
+  inactive: function () { bus.$emit('ytrc.font') }
+})
+
+/* vue */
+var ytrc = new Vue({
+  el: '#app',
+  data: {
+    config: {
+      channel: url('?channel') === undefined ? false : url('?channel') === '1',
+      desc: url('?desc') === undefined ? true : url('?desc') === '1',
+      link: url('?link') === undefined ? true : url('?link') === '1',
+      cpm: url('?cpm') === undefined ? false : url('?cpm') === '1',
+      highlight: url('?highlight') === undefined ? false : url('?highlight') === '1',
+      note: url('?note') === undefined ? true : url('?note') === '1',
+      detail: url('?detail') === undefined ? false : url('?detail') === '1',
+      title: url('?title') || '',
+      key: url('?key') || key || ''
+    },
+    state: {
+      init: false,
+      wait: false,
+      done: true,
+      tool: false,
+      help: false,
+      title: false,
+      key: false
+    },
+    link: {
+      path: '',
+      likes: 0,
+      subscribers: 0,
+      videos: 0,
+      views: 0,
+      channel: 'Unknown',
+      title: 'Unknown',
+      publish: 'Unknown',
+      default: 'Unknown'
+    },
+    slider: {
+      views: {
+        config: {
+          format: {
+            to: function (value) {
+              return Math.round(value)
+            },
+            from: Number
+          },
+          tooltips: {
+            to: function (value) {
+              return Math.round(value) + ' Views'
+            },
+            from: Number
+          },
+          // step: 100,
+          connect: [true, false],
+          range: {
+            'min': [0, 1],
+            '20%': [1000, 100],
+            '40%': [10000, 1000],
+            '60%': [100000, 10000],
+            '80%': [1000000, 100000],
+            'max': [10000000]
+          },
+          pips: {
+            mode: 'range',
+            density: 2.2
+          }
+        },
+        value: [1000]
+      },
+      cpm: {
+        config: {
+          format: {
+            to: function (value) {
+              return parseFloat(value.toFixed(2))
+            },
+            from: Number
+          },
+          tooltips: [{
+            to: function (value) {
+              return '$ ' + value.toFixed(2)
+            },
+            from: Number
+          }, {
+            to: function (value) {
+              return '$ ' + value.toFixed(2)
+            },
+            from: Number
+          }],
+          connect: true,
+          step: 0.01,
+          range: {
+            'min': 0,
+            'max': 10
+          },
+          pips: {
+            mode: 'positions',
+            density: 5,
+            values: [0, 20, 40, 60, 80, 100],
+            filter: function (value) {
+              return value % 1 ? (value % 0.5 ? 0 : 2) : 1
+            },
+            format: {
+              to: function (value) {
+                return '$ ' + value.toFixed(2)
+              },
+              from: Number
+            }
+          }
+        },
+        value: [1.36, 3.40]
+      }
+    },
+    api: {
+      base: 'https://www.googleapis.com/youtube/v3/',
+      channel: {
+        path: 'channels',
+        pattern: 'youtube.com/channel/',
+        example: 'https://www.youtube.com/channel/UCY_LMaDAoa6hwHKBE4Dx56w'
+      },
+      video: {
+        path: 'videos',
+        pattern: 'youtube.com/watch?v=',
+        example: 'https://www.youtube.com/watch?v=DqbcHgli0ik'
+      }
+    }
+  },
+  methods: {
+    togglePanel: function (panel, state) {
+      if (typeof this.state[panel] === 'boolean') {
+        if (typeof state === 'boolean') {
+          this.state[panel] = state
+        } else {
+          this.state[panel] = !this.state[panel]
+        }
+      }
+    },
+    toggleLink: function () {
+      // stackoverflow.com/questions/1714786/
+      var serialize = function (obj) {
+        var str = []
+        for (var p in obj) {
+          if (obj.hasOwnProperty(p)) {
+            var s = obj[p]
+            if (typeof s === 'boolean') s = s ? 1 : 0
+            str.push(encodeURIComponent(p) + '=' + encodeURIComponent(s))
+          }
+        }
+        return str.join('&')
+      }
+      var l = window.location
+      window.history.replaceState(null, null, l.protocol + '//' + l.host + l.pathname + '?' + serialize(this.config))
+    },
+    // calculate revennue
+    calculateRevenue: function (cpm, days) {
+      // views * cpm * days / 1000
+      return (this.link.views * (parseFloat(cpm) || this.slider.cpm.value[0]) * (parseInt(days) || 1) / 1000)
+    },
+    // format number
+    formatNumber: function (num, cur) {
+      return typeof num === 'number' ? num.toLocaleString('en-US', cur ? { minimumFractionDigits: 2, maximumFractionDigits: 2 } : undefined) : 0
+    },
+    // query youtube api for input link
+    queryYouTube: function () {
+      var t = this
+      var a = t.api[t.config.channel ? 'channel' : 'video']
+      var n = t.link.path.indexOf(a.pattern)
+      t.state.done = n > -1
+      if (t.state.done) {
+        t.state.wait = true
+        t.state.done = true
+        var id = t.link.path.substr(n + a.pattern.length)
+        axios({
+          method: 'get',
+          url: t.api.base + a.path,
+          params: {
+            part: 'snippet,contentDetails,statistics',
+            id: id,
+            key: t.config.key
+          }
+        }).then(function (res) {
+          // success
+          t.link.likes = parseInt(res.data.items[0].statistics.likeCount) || 0
+          t.link.views = parseInt(res.data.items[0].statistics.viewCount) || 0
+          t.link.videos = parseInt(res.data.items[0].statistics.videoCount) || 0
+          t.link.subscribers = parseInt(res.data.items[0].statistics.subscriberCount) || 0
+          t.link.title = res.data.items[0].snippet.title || t.link.default
+          t.link.channel = res.data.items[0].snippet.channelTitle || t.link.default
+          t.link.publish = new Date(res.data.items[0].snippet.publishedAt).toLocaleDateString('en-US') || t.link.default
+          t.state.done = true
+        }).catch(function (res) {
+          // fail
+          t.state.done = false
+          console.log(res)
+        }).then(function () {
+        // always
+          t.state.wait = false
+        })
+      }
+    }
+  },
+  created: function () {
+  },
+  mounted: function () {
+    // page ready when font loaded
+    bus.$on('ytrc.font', function () {
+      ytrc.state.init = true
+    })
+    // copy note
+    this.$refs['note-guide'].innerHTML = this.$refs['note-content'].innerHTML
+  },
+  watch: {
+    // update url when config changes
+    'config': {
+      handler: function () {
+        this.toggleLink()
+      },
+      deep: true,
+      immediate: true
+    },
+    // link counter with slider for views
+    'slider.views.value': {
+      handler: function (nv) {
+        this.link.views = nv[0]
+      },
+      immediate: true
+    },
+    // detail only for link and not for highlight
+    'config.detail': {
+      handler: function (nv) {
+        if (nv && (!this.config.link || this.config.highlight)) this.config.detail = false
+      },
+      immediate: true
+    },
+    'config.link': {
+      handler: function (nv) {
+        if (!nv) this.config.detail = false
+      },
+      immediate: true
+    },
+    'config.highlight': {
+      handler: function (nv) {
+        if (nv) this.config.detail = false
+      },
+      immediate: true
+    }
+  }
+})
